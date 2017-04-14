06cb15352cdc323ceb24f383885cf0aaf1b3dd6f
Ryan Beadie <ryansteelebeadie@yahoo.com>
2017-04-14 16:18:25 -0500

    init commit

--- practice_project/script.js
+++ practice_project/script.js
@@ -8,25 +8,46 @@
   you are also welcome to write more than the given functions.
 */
 var numberOfSquares = 10;
-
+var whichSquare= 0;
 $(document).ready(onReady);
 
 //Function that runs when html is fully loaded:
 function onReady(){
   //click listener to button:
-  $('.change-highlight').on('click', highlightASquare)
+  $('.change-highlight').on('click', highlightASquare);
 
   //call to put starting squares on DOM
   appendSquaresToDom(numberOfSquares);
 }
 
-function highlightASquare(index){
-  //This variable stores an array of the elements with the 'square' class
+function highlightASquare(){
   var arrayOfSquares = $('.square');
+  var onSquare = $(arrayOfSquares[whichSquare]);
+  var minusOnSquare=$(arrayOfSquares[whichSquare - 1]);
 
-}
+  if (whichSquare === 10) {
+      whichSquare=0;
+      $(arrayOfSquares[9]).removeClass("highlight");
+      $(arrayOfSquares[0]).addClass("highlight");
 
 
+  }//end if
+
+  //This variable stores an array of the elements with the 'square' class
+
+  onSquare.addClass("highlight");
+  whichSquare ++;
+  removeASquare();
+
+  function removeASquare(){
+    minusOnSquare.removeClass("highlight");
+
+
+  } //End removeASquare
+}//end highlightASquare
+
+
+//You don't need to change this function:
 function appendSquaresToDom(number){
   var squaresToAppend = "";
  //concatenate string of squares
